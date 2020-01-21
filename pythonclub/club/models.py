c212fdfde5140b32aa4f763aa2dad77482c52905
AggieWB <usmaggie23@gmail.com>
2020-01-21 09:14:21 -0800

    Add Meta subclass to set properties for the database and Admin app

--- pythonclub/club/models.py
+++ pythonclub/club/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Meeting(models.Model):
+    class Meta:
+        db_table="meeting"
+        verbose_name_plural="meetings"
 
 class MeetingMinutes(models.Model):
+    class Meta:
+        db_table="meetingminutes"
+        verbose_name_plural="meetingminutes"
 
 class Resource(models.Model):
+    class Meta:
+        db_table="resource"
+        verbose_name_plural="resources"
 
 class Event(models.Model):
+    class Meta:
+        db_table="event"
+        verbose_name_plural="events"
