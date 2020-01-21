9c0f00c2f96b2ee9256aa218851dc83fd542f4d5
AggieWB <usmaggie23@gmail.com>
2020-01-21 09:26:40 -0800

    Add text field field and charfield types

--- pythonclub/club/models.py
+++ pythonclub/club/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Meeting(models.Model):
-    meetingTitle
+    meetingTitle=models.Charfield(max_length=255)
     meetingDate
     meetingTime
-    location
-    agenda
+    location=models.Charfield(max_length=255)
+    agenda=models.Charfield(max_length=255)
 
     class Meta:
         db_table="meeting"
@@ -15,30 +15,30 @@ class Meeting(models.Model):
 class MeetingMinutes(models.Model):
     meetingID
     attendance
-    minutesText
+    minutesText=models.TextField()
 
     class Meta:
         db_table="meetingminutes"
         verbose_name_plural="meetingminutes"
 
 class Resource(models.Model):
-    resourceName
+    resourceName=models.Charfield(max_length=255)
     resourceType
     url
     dateEntered
     userID
-    description
+    description=models.Charfield(max_length=255)
 
     class Meta:
         db_table="resource"
         verbose_name_plural="resources"
 
 class Event(models.Model):
-    eventTitle
+    eventTitle=models.Charfield(max_length=255)
     location
     date
     time
-    description
+    description=models.TextField()
     userID
 
     class Meta:
