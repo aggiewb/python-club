88032bff2be55c5c1d612d5674072595da7c6efb
AggieWB <usmaggie23@gmail.com>
2020-01-27 09:50:19 -0800

    Create views for all of the model data

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -4,3 +4,19 @@ from .models import Meeting, MeetingMinutes, Resource, Event
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+def getMeeting(request):
+    type_list = Meeting.objects.all()
+    return render(request, 'club/meeting.html', {'type_list' : type_list})
+
+def getMeetingMinutes(request):
+    type_list = MeetingMinutes.objects.all()
+    return render(request, 'club/meeting_minutes.html', {'type_list' : type_list})
+
+def getResource(request):
+    type_list = Resource.objects.all()
+    return render(request, 'club/resource.html', {'type_list' : type_list})
+
+def getEvent(request):
+    type_list = Event.objects.all()
+    return render(request, 'club/event.html', {'type_list' : type_list}) 
