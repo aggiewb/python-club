93090e55dd0e64bd1fbabc13ca1d8d0eeae08941
AggieWB <usmaggie23@gmail.com>
2020-01-28 11:18:05 -0800

    Change curly brace to parenthesis to correct syntax

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('getMeeting/', views.getMeeting, name='meeting'), 
-    path{'getMeetingMinutes/', views.getMeetingMinutes, name='meeting_minutes'},
-    path{'getResource/', views.getResource, name='resource'},
-    path{'getEvent', views.getEvent, name='event'},
+    path('getMeetingMinutes/', views.getMeetingMinutes, name='meeting_minutes'),
+    path('getResource/', views.getResource, name='resource'),
+    path('getEvent', views.getEvent, name='event'),
 ]
