fe4a32ab42aa57253186efa0b176b0c3b0571bbc
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-01-29 13:22:00 -0800

    Update secret key, database password, and host to be hidden

--- pythonclub/pythonclub/settings.py
+++ pythonclub/pythonclub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qq1(h7=cxtnky^vecaq4#g8#@=h&u!7ky624f1vlp(fmyczo45'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'pythonclub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pythonclub',
-        'USER' : 'Aggiewb',
-        'PASSWORD': 'Bannana623!',
-        'HOST' :'localhost',
+        'NAME': config('DB_NAME'),
+        'USER' : config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST' : config('DB_HOST'),
         'PORT' :'5432',
     }
 }
