1d305185408c09b7c5af8a506ef41cd2c9205105
AggieWB <usmaggie23@gmail.com>
2020-02-03 22:07:46 -0800

    Replace meetingMinutes url pattern with meetingDetails

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('meeting/', views.meeting, name='meeting'), 
-    path('meeting_minutes/', views.meetingMinutes, name='meeting_minutes'),
+    path('meeting_details/', views.meetingDetails, name='meeting_details'),
     path('resource/', views.resource, name='resource'),
     path('event/', views.event, name='event'),
 ]
