3d6c82b7b62e71358ce50b105a2f7558a03d9e40
AggieWB <usmaggie23@gmail.com>
2020-02-03 22:17:07 -0800

    Add the meeting id to the url pattern for meeting_details

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('meeting/', views.meeting, name='meeting'), 
-    path('meeting_details/', views.meetingDetails, name='meeting_details'),
+    path('meeting_details/<int:id>', views.meetingDetails, name='meeting_details'),
     path('resource/', views.resource, name='resource'),
     path('event/', views.event, name='event'),
 ]
