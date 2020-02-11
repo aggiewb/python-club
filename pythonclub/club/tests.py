121a389057d851b949603ad19568797286d21a43
AggieWB <usmaggie23@gmail.com>
2020-02-11 09:00:15 -0800

    Create test for Meeting class for the __str__ function return

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -1,4 +1,16 @@
 from django.test import TestCase
 from .models import Meeting, MeetingMinutes, Resource, Event
 
-# Create your tests here.
+class MeetingTest(TestCase):
+    def test_string(self):
+        meetingTitle=Meeting(meetingTitle="Annual Meeting")
+        meetingDate=Meeting(meetingDate="2020-06-23") 
+        meetingTime=Meeting(meetingTime="03:30 PM")
+        location=Meeting(location="Community Center")
+        agenda=Meeting(agenda="Vote new board members")
+        self.assertEqual(str(meetingTitle), meetingTitle.meetingTitle)
+        self.assertEqual(str(meeetingDate), meetingDate.meetingDate)
+        self.assertEqual(str(meetingTime), meetingTime.meetingTime)
+        self.assertEqual(str(location), location.location)
+        self.assertEqual(str(agenda), agenda.agenda)
+
