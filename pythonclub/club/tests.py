6fce63a727f3aeaeb5221dce1b4be2bd5204883e
AggieWB <usmaggie23@gmail.com>
2020-02-11 09:48:18 -0800

    Change double quotes to single quotes

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -3,11 +3,11 @@ from .models import Meeting, MeetingMinutes, Resource, Event
 
 class MeetingTest(TestCase):
     def test_string(self):
-        meetingTitle=Meeting(meetingTitle="Annual Meeting")
-        meetingDate=Meeting(meetingDate="2020-06-23") 
-        meetingTime=Meeting(meetingTime="03:30 PM")
-        location=Meeting(location="Community Center")
-        agenda=Meeting(agenda="Vote new board members")
+        meetingTitle=Meeting(meetingTitle='Annual Meeting')
+        meetingDate=Meeting(meetingDate='2020-06-23') 
+        meetingTime=Meeting(meetingTime='03:30 PM')
+        location=Meeting(location='Community Center')
+        agenda=Meeting(agenda='Vote new board members')
         self.assertEqual(str(meetingTitle), meetingTitle.meetingTitle)
         self.assertEqual(str(meetingDate), meetingDate.meetingDate)
         self.assertEqual(str(meetingTime), meetingTime.meetingTime)
@@ -16,18 +16,18 @@ class MeetingTest(TestCase):
 
 class MeetingMinutesTest(TestCase):
     def test_string(self):
-        attendance=MeetingMinutes(attendance="Bob Boberton")
-        minutesText=MeetingMinutes(minutesText="Susie Sue was voted in as board president")
+        attendance=MeetingMinutes(attendance='Bob Boberton')
+        minutesText=MeetingMinutes(minutesText='Susie Sue was voted in as board president')
         self.assertEqual(str(attendance), attendance.attendance)
         self.assertEqual(str(minutesText), minutesText.minutesText)
 
 class ResourceTest(TestCase):
     def test_string(self):
-        resourceName=Resource(resourceName="Offical Documentation")
-        resourceType=Resource(resourceType="Django")
-        url=Resource(url="https://docs.djangoproject.com/en/3.0/")
-        dateEntered=Resource(dateEntered="2020-09-02")
-        description=Resource(description="This is the offical documentation on Django")
+        resourceName=Resource(resourceName='Offical Documentation')
+        resourceType=Resource(resourceType='Django')
+        url=Resource(url='https://docs.djangoproject.com/en/3.0/')
+        dateEntered=Resource(dateEntered='2020-09-02')
+        description=Resource(description='This is the offical documentation on Django')
         self.assertEqual(str(resourceName), resourceName.resourceName)
         self.assertEqual(str(resourceType), resourceType.resourceType)
         self.assertEqual(str(url), url.url)
@@ -36,11 +36,11 @@ class ResourceTest(TestCase):
 
 class EventTest(TestCase):
     def test_string(self):
-        eventTitle=Event(eventTitle="PyDay")
-        location=Event(location="Programmers Ale House")
-        date=Event(date="2020-02-21")
-        time=Event(time="06:30 PM")
-        description=Event(description="A day to celebrate Python!")
+        eventTitle=Event(eventTitle='PyDay')
+        location=Event(location='Programmers Ale House')
+        date=Event(date='2020-02-21')
+        time=Event(time='06:30 PM')
+        description=Event(description='A day to celebrate Python!')
         self.assertEqual(str(eventTitle), eventTitle.eventTitle)
         self.assertEqual(str(location), location.location)
         self.assertEqual(str(date), date.date)
