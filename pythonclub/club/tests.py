9db90ac059bf997bd17898602aadfa4d780e1b49
AggieWB <usmaggie23@gmail.com>
2020-02-11 16:15:53 -0800

    Add test class for resource view url

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -50,6 +50,11 @@ class IndexTest(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
+class ResourceViewTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('resource'))
+        self.assertEqual(response.status_code, 200)
+
 
 
 
