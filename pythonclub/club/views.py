e614d0ddb1c377599ff9ef6010d56a8d6e23c695
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-02-19 13:36:28 -0800

    Remove decorator

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
-from django.contrib.auth.decorator import login_required
 
 # Create your views here.
 def index (request):
