bec686de384e6ecbaed5155df614af57459d5175
AggieWB <usmaggie23@gmail.com>
2020-02-24 08:21:31 -0800

    Correct syntax/spelling errors

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('resource/', views.resource, name='resource'),
     path('event/', views.event, name='event'),
     path('loginmessage/', views.loginMessage, name='loginmessage'), 
-    path('logoutmessage/' views.logoutMesage, name='logoutmessage')
+    path('logoutmessage/', views.logoutMessage, name='logoutmessage'),
     path('newMeeting/', views.newMeeting, name='newmeeting'),
     path('newResource/', views.newResource, name='newresource')
 
