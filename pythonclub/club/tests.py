142e9c2508aeb3194878bc7784371b79fb29ab28
AggieWB <usmaggie23@gmail.com>
2020-02-24 09:47:14 -0800

    Add tests for if logged in user uses correct template
    After user log in, test for form access and if url response is 200

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -123,6 +123,13 @@ class NewMeetingAuthTest(TestCase):
         response=self.client.get(reverse('newmeeting'))
         self.assertRedirects(response, '/accounts/login/?next=/club/newMeeting/')
 
+    def test_Logged_in_uses_correct_template(self):
+        login=self.client.login(username='testuser1', password='P@ssw0rd1')
+        response=self.client.get(reverse('newmeeting'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club/newmeeting.html')
+
 class NewResourceAuthTest(TestCase):
     def setUp(self):
         self.test_user= User.objects.create_user(username='testuser2', password='P@ssw0rd2')
